c89dc965a89409a3e9a264b1ae15c22241fa5131
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-12 20:12:34 -0700

    commit tree 3efe1021518f41eb317323379c5c0b8220b4463e

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer ="billy"
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+     bestCustomer = "not bob";
+
+}
+
+//console.log(upperCaseCustomerName())
+
+
+
+function overwriteBestCustomer(){
+     bestCustomer = "maybe bob";
+ }
+
+
+ function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'Assignment to constant variable.'
+    return leastFavoriteCustomer;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
